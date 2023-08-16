d085ac5baf8485dd3c961135df1384436e733c46
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-08-16 14:17:38 -0700

    dss script: try different factors

--- scripts/DSS.R
+++ scripts/DSS.R
@@ -34,13 +34,26 @@ create_model_two_table <- function(case_names, allele_names, family_names) {
   return(df)
 }
 
-test_model_one <- function(bs_object, design, output_prefix) {
+test_model_one <- function(bs_object, design, output_prefix, which_base) {
 
     design[] <- lapply(design, factor)
+
+    # Define the factor order
+    if (which_base == "sibling") {
+        design$case <- factor(design$case, levels=c("sibling", "mother", "asd"))
+        coef1_name="casemother"
+    else if (which_base == "mother") {
+        design$case <- factor(design$case, levels=c("mother", "sibling", "asd"))
+        coef1_name="casesibling"
+    } else {
+        warning(paste(which_base, "is an unsupported argument.", sep=" "))
+        stop("Halting the code due to unsupported argument.")
+    }
 
     DMLfit <- DMLfit.multiFactor(BSobj, design=design, formula=~case+family.id)
 
     print(DMLfit$X)
+    cat("\n")
 
     DMLtest.case <- DMLtest.multiFactor(DMLfit, term="case")
     DMLtest.familyid <- DMLtest.multiFactor(DMLfit, term="family.id")
@@ -54,8 +67,8 @@ test_model_one <- function(bs_object, design, output_prefix) {
 
     write.table(DMLtest.case[case.ix,], file=paste(output_prefix, "case_DML.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
     write.table(DMLtest.familyid[familyid.ix,], file=paste(output_prefix, "familyid_DML.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
-    write.table(DMLtest.case.coef1[case.coef1.ix,], file=paste(output_prefix, "case-coef1_DML.tsv", sep="_"), sep="\t", quote=FALSE, row.names=FALSE)
-    write.table(DMLtest.case.coef2[case.coef2.ix,], file=paste(output_prefix, "case-coef2_DML.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
+    write.table(DMLtest.case.coef1[case.coef1.ix,], file=paste(output_prefix, coef1_name, "DML.tsv", sep="_"), sep="\t", quote=FALSE, row.names=FALSE)
+    write.table(DMLtest.case.coef2[case.coef2.ix,], file=paste(output_prefix, "caseasd_DML.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
 
     call.case.dmr <- callDMR(DMLtest.case, p.threshold=0.001)
     call.familyid.dmr <- callDMR(DMLtest.familyid, p.threshold=0.001)
@@ -64,8 +77,8 @@ test_model_one <- function(bs_object, design, output_prefix) {
 
     write.table(call.case.dmr, file=paste(output_prefix, "case_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
     write.table(call.familyid.dmr, file=paste(output_prefix, "familyid_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
-    write.table(call.case.coef1.dmr, file=paste(output_prefix, "case-coef1_DMR.tsv", sep="_"), sep="\t", quote=FALSE, row.names=FALSE)
-    write.table(call.case.coef2.dmr, file=paste(output_prefix, "case-coef2_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
+    write.table(call.case.coef1.dmr, file=paste(output_prefix, coef1_name, "DMR.tsv", sep="_"), sep="\t", quote=FALSE, row.names=FALSE)
+    write.table(call.case.coef2.dmr, file=paste(output_prefix, "caseasd_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
 }
 
 test_model_two <- function(bs_object, design, output_prefix) {
@@ -75,6 +88,7 @@ test_model_two <- function(bs_object, design, output_prefix) {
     DMLfit <- DMLfit.multiFactor(BSobj, design=design, formula=~case+allele+family.id)
 
     print(DMLfit$X)
+    cat("\n")
 
     DMLtest.case <- DMLtest.multiFactor(DMLfit, term="case")
     DMLtest.familyid <- DMLtest.multiFactor(DMLfit, term="family.id")
@@ -88,9 +102,9 @@ test_model_two <- function(bs_object, design, output_prefix) {
     write.table(DMLtest.familyid[familyid.ix,], file=paste(output_prefix, "familyid_DML.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
     write.table(DMLtest.allele[allele.ix,], file=paste(output_prefix, "allele_DML.tsv", sep="_"), sep="\t", quote=FALSE, row.names=FALSE)
 
-    call.case.dmr <- callDMR(DMLtest.case, p.threshold=0.05)
-    call.familyid.dmr <- callDMR(DMLtest.familyid, p.threshold=0.05)
-    call.allele.dmr <- callDMR(DMLtest.allele, p.threshold=0.05)
+    call.case.dmr <- callDMR(DMLtest.case, p.threshold=0.001)
+    call.familyid.dmr <- callDMR(DMLtest.familyid, p.threshold=0.001)
+    call.allele.dmr <- callDMR(DMLtest.allele, p.threshold=0.001)
 
     write.table(call.case.dmr, file=paste(output_prefix, "case_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
     write.table(call.familyid.dmr, file=paste(output_prefix, "familyid_DMR.tsv", sep="_"),sep="\t", quote=FALSE, row.names=FALSE)
@@ -114,11 +128,14 @@ BSobj <- process_files(file.names, param_dict$sample_names)
 if (param_dict$model == "model_one") {
     design.table <- create_model_one_table(case_names = param_dict$case, family_names = param_dict$family_names)
     print(design.table)
+    cat("\n")
     test_model_one(bs_object = BSobj, design = design.table, output_prefix = output.prefix)
 } else if (param_dict$model == "model_two") {
     design.table <- create_model_two_table(case_names = param_dict$case, allele_names= param_dict$allele_names, family_names = param_dict$family_names)
     print(design.table)
+    cat("\n")
     test_model_two(bs_object = BSobj, design = design.table, output_prefix = output.prefix)
 } else {
     warning(paste(param_dict$model, "has unsupported argument.", sep=" "))
+    stop("Halting the code due to unsupported argument.")
 }
